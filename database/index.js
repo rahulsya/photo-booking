0db05c9624fc483a2cd7b5f56b688f1fc7a47c6b
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-11-02 23:22:49 +0700

    change connection to production

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`${process.env.MONGODB_URI_DEV}`);
+mongoose.connect(`${process.env.MONGODB_URI_PROD}`);
 
 const db = mongoose.connection;
 module.exports = db;
