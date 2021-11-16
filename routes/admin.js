dc13ed75b5b387bb07afcee41cc108b871577a48
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-11-17 00:10:49 +0700

    add new route categories

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,35 @@
 const router = require("express").Router();
-const { adminContoller, userContoller } = require("../controller");
+const {
+  adminContoller,
+  userContoller,
+  categoriesContoller,
+  LoginController,
+} = require("../controller");
 const multer = require("multer");
 const { uploadSingel } = require("../middleware/multer");
-
-router.get("/", adminContoller.viewDashboard);
+const auth = require("../middleware/auth");
 router.get("/signin", (req, res) => {
-  return res.render("index", { title: "Login" });
+  if (!req.session.user) {
+    return res.render("index", { title: "Login" });
+  }
+  return res.redirect("/admin");
 });
+router.post("/signin", multer().none(), LoginController.actionLogin);
 
+router.use(auth);
+router.get("/logout", LoginController.actionLogout);
+router.get("/", adminContoller.viewDashboard);
 // photos
 router.get("/photos", adminContoller.viewPhoto);
 router.put("/photo", uploadSingel, adminContoller.editPhoto);
 router.post("/photo", uploadSingel, adminContoller.addPhoto);
 router.delete("/photo/:id", adminContoller.deletePhoto);
+
+// categories
+router.get("/categories", categoriesContoller.viewCategories);
+router.post("/categories", uploadSingel, categoriesContoller.addCategories);
+router.put("/categories", uploadSingel, categoriesContoller.editCategories);
+router.delete("/categories/:id", categoriesContoller.deleteCategories);
 
 // users
 router.get("/users", userContoller.viewUsers);
