e83445cb45f5cd79f5894c42c43746bb0015a056
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-11-17 00:10:27 +0700

    register controller login and categories

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,11 @@
 const adminContoller = require("./admin/photosController");
 const userContoller = require("./admin/UsersContoller");
+const categoriesContoller = require("./admin/CategoriesController");
+const LoginController = require("./admin/LoginController");
 
 module.exports = {
   adminContoller,
   userContoller,
+  categoriesContoller,
+  LoginController,
 };
