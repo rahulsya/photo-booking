5f9978c5fed08084b31df0830c56c14eb4824464
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-12-06 20:28:01 +0700

    register controller

--- controller/index.js
+++ controller/index.js
@@ -2,10 +2,24 @@ const adminContoller = require("./admin/photosController");
 const userContoller = require("./admin/UsersContoller");
 const categoriesContoller = require("./admin/CategoriesController");
 const LoginController = require("./admin/LoginController");
+const BookingController = require("./admin/BookingController");
+
+//api
+const ApiBookingController = require("./api/ApiBookingController");
+const ApiLoginController = require("./api/ApiLoginController");
+const ApiPhotoController = require("./api/ApiPhotoController");
+const ApiKategoriController = require("./api/ApiKategoriController");
 
 module.exports = {
   adminContoller,
   userContoller,
   categoriesContoller,
   LoginController,
+  BookingController,
+
+  //api
+  ApiBookingController,
+  ApiLoginController,
+  ApiPhotoController,
+  ApiKategoriController,
 };
