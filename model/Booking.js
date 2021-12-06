81606ce7407ba7c14e3949e21888c9c221b6db4f
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-12-06 20:29:04 +0700

    add modal booking

---
+++ model/Booking.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const BookingSchema = Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "category",
+    },
+    date_start: {
+      type: Date,
+      required: true,
+    },
+    session: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image_payment_url: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("booking", BookingSchema);
