65dd316ab9e68edf7d0418812beb83a94bff423c
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-12-06 20:31:34 +0700

    add router api

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
+const multer = require("multer");
+const { uploadSingel } = require("../middleware/multer");
+const {
+  ApiLoginController,
+  ApiPhotoController,
+  ApiKategoriController,
+  ApiBookingController,
+} = require("../controller");
 
 router.get("/", (req, res) => {
   return res.json({
@@ -6,5 +14,20 @@ router.get("/", (req, res) => {
     message: "this api",
   });
 });
+router.get("/me", (req, res) => {
+  return res.json({
+    status: "success",
+    user: req.user,
+  });
+});
+router.get("/photos", ApiPhotoController.Photos);
+router.get("/categories", ApiKategoriController.categories);
+
+router.post("/login", multer().none(), ApiLoginController.login);
+router.post("/register", multer().none(), ApiLoginController.register);
+
+// booking
+router.get("/booking", multer().none(), ApiBookingController.allBooking);
+router.post("/booking", uploadSingel, ApiBookingController.store);
 
 module.exports = router;
