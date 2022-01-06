af850b0af3e30075e1643174176585edd584b859
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-01-06 18:27:15 +0700

    add flash message status and permission to login to admin page

--- controller/admin/LoginController.js
+++ controller/admin/LoginController.js
@@ -6,11 +6,22 @@ module.exports = {
     try {
       const { username, password } = req.body;
       let user = await User.findOne({ name: username });
+
       if (!user) {
+        req.flash("alertMessage", "User Not Found");
+        req.flash("alertStatus", "danger");
         return res.redirect("/admin/signin");
       }
-      const checkPassword = await bcrypt.compare(password, user.password);
+      const checkPassword = bcrypt.compareSync(password, user.password);
       if (!checkPassword) {
+        req.flash("alertMessage", "Wrong User Or Password");
+        req.flash("alertStatus", "danger");
+        return res.redirect("/admin/signin");
+      }
+
+      if (user.role === "user") {
+        req.flash("alertMessage", `Role "User" Not Allowed`);
+        req.flash("alertStatus", "danger");
         return res.redirect("/admin/signin");
       }
 
@@ -18,8 +29,10 @@ module.exports = {
         id: user.id,
         name: user.name,
       };
+
       return res.redirect("/admin");
     } catch (error) {
+      console.log(error.message);
       return res.redirect("/admin/signin");
     }
   },
